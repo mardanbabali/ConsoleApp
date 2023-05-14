437bc10562dd5d2690e402140b29d351356ff566
Mardan <mardanvb@code.edu.az>
2023-05-15 00:14:15 +0400

    consoleapp

---
+++ fesiller/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("ay adini daxil edin:");
+string Month = Console.ReadLine();
+
+if (Month == "yanvar" ||Month== "fevral" || Month=="dekabr")
+{
+    Console.WriteLine("winter");
+}
+else if (Month == "mart" || Month == "aprel" || Month == "may")
+{
+    Console.WriteLine("Spring");
+}
